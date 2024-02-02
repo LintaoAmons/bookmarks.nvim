c2a8056c663fe008cebc9769799725caa7cd3c3b
Lintao <lintao.amons@outlook.com>
2024-02-02 22:34:51 +0800

    define domain type

--- lua/bookmarks/project-repo.lua
+++ lua/bookmarks/project-repo.lua
@@ -1,13 +1,27 @@
 local json = require("bookmarks.json")
----@class PluginName.Domain
----@field name string
 
----@return PluginName.Domain[]
+---@class Bookmarks.Location
+---@field path string
+---@field line number
+---@field col number
+
+---@class Bookmarks.Bookmark
+---@field name string
+---@field location Bookmarks.Location
+---@field content string
+---@field githash string
+
+---@class Bookmarks.BookmarkList
+---@field id string
+---@field name string
+---@field bookmarks Bookmarks.Bookmark[]
+--
+---@return Bookmarks.BookmarkList[]
 local get_domains = function()
 	return json.read_or_init_json_file(vim.g.bookmarks_config.projects_config_filepath)
 end
 
----@param domain PluginName.Domain[]
+---@param domain Bookmarks.BookmarkList[]
 local write_domains = function(domain)
 	json.write_json_file(domain, vim.g.bookmarks_config.projects_config_filepath)
 end
