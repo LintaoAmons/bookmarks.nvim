cfa214aa4b3c6fa1aa43f4e8a2d4bd2829f3387e
Lintao <lintao.amons@outlook.com>
2024-02-02 23:12:24 +0800

    wip

--- lua/bookmarks/api.lua
+++ lua/bookmarks/api.lua
@@ -1,7 +1,38 @@
-local function api_name()
-	vim.print(vim.g.bookmarks_config.json_db_path)
+local repo = require("bookmarks.project-repo")
+
+---@class Bookmarks.MarkParam
+---@field name string
+
+---@param param Bookmarks.MarkParam
+local function mark(param)
+	-- 1. build bookmark
+	local cursor = vim.api.nvim_win_get_cursor(0)
+	local filename = vim.fn.expand("%:p")
+	---@type Bookmarks.Bookmark
+	local bookmark = {
+		name = param.name,
+		location = { path = filename, line = cursor[0], col = cursor[1] },
+		content = "content", -- TODO: check if current worktree's line content match this value
+		githash = "githash", -- TODO: if not match, notify user with the githash.
+	}
+
+	-- 2. get active bookmark list
+	local active_bookmark_list = repo.find_or_set_active_bookmark_list()
+  print('DEBUGPRINT[1]: api.lua:20: active_bookmark_list=' .. vim.inspect(active_bookmark_list))
+	-- 3. insert into bookmark list
+  print('DEBUGPRINT[2]: api.lua:23: active_bookmark_list.bookmarks=' .. vim.inspect(active_bookmark_list.bookmarks))
+  active_bookmark_list.bookmarks = vim.tbl_deep_extend("force", active_bookmark_list.bookmarks, bookmark)
+  print('DEBUGPRINT[3]: api.lua:22: active_bookmark_list.bookmarks=' .. vim.inspect(active_bookmark_list))
+
 end
 
+---@class Bookmarks.NewListParam
+---@field name string
+
+---@param param Bookmarks.NewListParam
+local function new_list(param) end
+
 return {
-	api_name = api_name,
+	mark = mark,
+	new_list = new_list,
 }
--- lua/bookmarks/project-repo.lua
+++ lua/bookmarks/project-repo.lua
@@ -14,19 +14,76 @@ local json = require("bookmarks.json")
 ---@class Bookmarks.BookmarkList
 ---@field id string
 ---@field name string
+---@field is_active boolean
 ---@field bookmarks Bookmarks.Bookmark[]
---
+
 ---@return Bookmarks.BookmarkList[]
 local get_domains = function()
-	return json.read_or_init_json_file(vim.g.bookmarks_config.projects_config_filepath)
+	print(
+		"DEBUGPRINT[3]: project-repo.lua:23: vim.g.bookmarks_config.projects_config_filepath="
+			.. vim.inspect(vim.g.bookmarks_config.json_db_path)
+	)
+	return json.read_or_init_json_file(vim.g.bookmarks_config.json_db_path)
 end
 
 ---@param domain Bookmarks.BookmarkList[]
 local write_domains = function(domain)
-	json.write_json_file(domain, vim.g.bookmarks_config.projects_config_filepath)
+	json.write_json_file(domain, vim.g.bookmarks_config.json_db_path)
+end
+
+-- Function to generate an ID in the datetime format
+local function generate_datetime_id()
+	-- Get the current date and time in the desired format
+	local datetime = os.date("%Y%m%d%H%M%S")
+
+	-- Generate a random number (e.g., using math.random) and append it to the datetime
+	local random_suffix = ""
+	for _ = 1, 8 do
+		random_suffix = random_suffix .. tostring(math.random(0, 9))
+	end
+
+	-- Concatenate the datetime and random suffix to create the ID
+	local id = datetime .. random_suffix
+
+	return id
+end
+
+---@return Bookmarks.BookmarkList
+local function find_or_set_active_bookmark_list()
+	local bookmark_lists = get_domains()
+	print("DEBUGPRINT[1]: project-repo.lua:49: get_domains=" .. vim.inspect(get_domains))
+	local active_bookmark_list = nil
+
+	-- Check if there's an active BookmarkList
+	for _, bookmark_list in ipairs(bookmark_lists) do
+		if bookmark_list.is_active then
+			active_bookmark_list = bookmark_list
+			break
+		end
+	end
+
+	-- If no active BookmarkList was found, mark the first one as active
+	if not active_bookmark_list and #bookmark_lists > 0 then
+		bookmark_lists[1].is_active = true
+		active_bookmark_list = bookmark_lists[1]
+	end
+
+	-- If there are no BookmarkLists, create a new one
+	if not active_bookmark_list then
+		active_bookmark_list = {
+			id = generate_datetime_id(),
+			name = "Default",
+			is_active = true,
+			bookmarks = {},
+		}
+		table.insert(bookmark_lists, active_bookmark_list)
+	end
+
+	return active_bookmark_list
 end
 
 return {
 	get_domains = get_domains,
 	write_domains = write_domains,
+	find_or_set_active_bookmark_list = find_or_set_active_bookmark_list,
 }
