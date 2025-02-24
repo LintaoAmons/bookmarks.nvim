2798806d10cb89c057e7e8e394f74d7e7ed6cb9f
Lintao <lintao.amons@outlook.com>
2025-02-24 22:40:11 +0800

    fix(auto-cmd): correct condition check for auto-calibration

--- lua/bookmarks/auto-cmd.lua
+++ lua/bookmarks/auto-cmd.lua
@@ -4,12 +4,12 @@ local GROUP_NAME = "BookmarksNvimAutoCmd"
 M.setup = function()
   vim.api.nvim_create_augroup(GROUP_NAME, { clear = true })
 
-  vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+  vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter", "InsertLeave" }, {
     pattern = { "*" },
     group = GROUP_NAME,
 
     callback = function()
-      if vim.g.bookmarks_config and vim.g.calibrate and vim.g.bookmarks_config.calibrate.auto_calibrate_cur_buf then
+      if vim.g.bookmarks_config and vim.g.bookmarks_config.calibrate and vim.g.bookmarks_config.calibrate.auto_calibrate_cur_buf then
         require("bookmarks.calibrate").calibrate_current_window()
         require("bookmarks.sign").safe_refresh_signs()
         pcall(require("bookmarks.tree.operate").refresh)
